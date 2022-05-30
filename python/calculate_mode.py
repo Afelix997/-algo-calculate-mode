fba16c33a7b158bea6dca34605dddc11c0c403ae
afelix997 <afelix2013@gmail.com>
2022-05-30 15:12:02 -0700

    answer

--- python/calculate_mode.py
+++ python/calculate_mode.py
@@ -1,2 +1,13 @@
-def calculate_mode():
-    pass
+def calculate_mode(input_arr):
+    answer=[]
+    obj={}
+    for value in input_arr:
+        if value in obj.keys():
+            obj[value]+=1
+        else:
+            obj[value]=1
+    most_rep = max(obj, key=obj.get)
+    for key in obj:
+        if obj[key] >= obj[most_rep]:
+            answer.append(key)
+    return answer
