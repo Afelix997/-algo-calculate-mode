aa2d45b094787cfe9271b5db697a3019f453bb34
afelix997 <afelix2013@gmail.com>
2022-05-30 14:55:42 -0700

    answer

--- js/calculateMode.js
+++ js/calculateMode.js
@@ -1,3 +1,21 @@
-const calculateMode = () => {
-
+const calculateMode = (inputArr) => {
+    let answer=[]
+    let obj={}
+    let mostRep=inputArr[0];
+    inputArr.forEach(value => {
+        if (obj[value]){
+        obj[value] +=1
+    }else{
+        obj[value]=1
+    }})
+    for (let key in obj)
+        if (obj[key] > obj[mostRep]){
+            mostRep = key;
+        }
+    for (let key in obj){
+        if (obj[key] >= obj[mostRep]){
+            answer.push(key)
+    }}
+    return answer
 }
+
